7b703d23fcbf78fdc719ce714d23ec96b7245b6a
Meija Lohiniva <mlohiniva@hotmail.com>
2021-10-19 12:59:21 +0300

    Välisave

--- forms.js
+++ forms.js
@@ -1,3 +1,38 @@
+function showErrorMessage(element) {
+    // Virheviesti jos tyhjä
+    console.log("Tyhjä kenttä");
+    element.classList.add('red');
+    element.parentElement.parentElement.getElementsByClassName('errorMessage')[0].style="display:inline";
+    // Asetetaan flagimuuttuja falseksi
+    return false;
+}
+function hideErrorMessage(element) {
+    element.classList.remove('red');
+    element.parentElement.parentElement.getElementsByClassName('errorMessage')[0].style="display: none;";
+}
+function checkElement(element) {
+// Tarkistetaan käsiteltävänä olevan elementin sisältö
+        // Tarkista minkätyyppinen elementti kyseessä
+        if (element instanceof HTMLSelectElement) {
+            // Jos select -> tarkistetaan onko valinta --Select gender--
+            if(element.selectedIndex == 0) {
+                // Valintaa ei ole tehty, valittuna ensimmäinen vaihtoehto -> näytä virheilmoitus
+                inputsOK = showErrorMessage(element);
+            } else {
+                // Kenttä ok -> piilotetaan punaiset kehykset ja virheviesti
+                hideErrorMessage(element);
+            }
+        } else {
+            // Jos input -> tarkistetaan onko value tyhjä
+            if (element.value == '') {
+                // Virheviesti jos tyhjä
+                inputsOK = showErrorMessage(element);
+            } else {
+                // Kenttä ok -> piilotetaan punaiset kehykset ja virheviesti
+                hideErrorMessage(element);
+            }
+        }
+}
 document.getElementById('submitButton').onclick = function() {
     // Haetaan elementit
     let elements = document.getElementsByClassName('required');
@@ -5,19 +40,7 @@ document.getElementById('submitButton').onclick = function() {
     let inputsOK = true;
     // Loopataan elementit läpi
     for (let i = 0; i < elements.length; i++) {
-        // Tarkistetaan käsiteltävänä olevan elementin sisältö
-        if (elements[i].value == '') {
-            // Virheviesti jos tyhjä
-            console.log("Tyhjä kenttä");
-            elements[i].classList.add('red');
-            elements[i].parentElement.parentElement.getElementsByClassName('errorMessage')[0].style="display:inline";
-            // Asetetaan flagimuuttuja falseksi
-            inputsOK = false;
-        } else {
-            // Kenttä ok -> piilotetaan punaiset kehykset ja virheviesti
-            elements[i].classList.remove('red');
-            elements[i].parentElement.parentElement.getElementsByClassName('errorMessage')[0].style="display: none;";
-        }
+        checkElement(elements[i]);
     }
     return inputsOK;
 }
